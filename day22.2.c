d3146eabfb68ae16b8e837f6c3549969dc90d959
codermoksh <mokshjain012@gmail.com>
2025-10-25 19:20:58 +0530

    Update day22.2.c

--- day22.2.c
+++ day22.2.c
@@ -1,4 +1,4 @@
-/* Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms. */
+/*Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms. */
 
 #include <stdio.h>
 
