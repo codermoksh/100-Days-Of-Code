0f3bd23b204bc0139b8e3f8884cf2c1539e9fa11
codermoksh <mokshjain012@gmail.com>
2025-10-31 21:33:03 +0530

    Add files via upload

---
+++ day50.1.c
@@ -0,0 +1,37 @@
+/*Q99: Change the date format from dd/04/yyyy to dd-Apr-yyyy.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char date[15], day[3], month[3], year[5];
+    char *monthName;
+
+    printf("Enter date (dd/mm/yyyy): ");
+    scanf("%s", date);
+
+    strncpy(day, date, 2);
+    day[2] = '\0';
+    strncpy(month, date + 3, 2);
+    month[2] = '\0';
+    strcpy(year, date + 6);
+
+    if (strcmp(month, "01") == 0) monthName = "Jan";
+    else if (strcmp(month, "02") == 0) monthName = "Feb";
+    else if (strcmp(month, "03") == 0) monthName = "Mar";
+    else if (strcmp(month, "04") == 0) monthName = "Apr";
+    else if (strcmp(month, "05") == 0) monthName = "May";
+    else if (strcmp(month, "06") == 0) monthName = "Jun";
+    else if (strcmp(month, "07") == 0) monthName = "Jul";
+    else if (strcmp(month, "08") == 0) monthName = "Aug";
+    else if (strcmp(month, "09") == 0) monthName = "Sep";
+    else if (strcmp(month, "10") == 0) monthName = "Oct";
+    else if (strcmp(month, "11") == 0) monthName = "Nov";
+    else if (strcmp(month, "12") == 0) monthName = "Dec";
+    else monthName = "Invalid";
+
+    printf("%s-%s-%s\n", day, monthName, year);
+
+    return 0;
+}
---
+++ day50.2.c
@@ -0,0 +1,26 @@
+/*Q100: Print all sub-strings of a string.*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, k;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = i; str[j] != '\0'; j++)
+        {
+            for (k = i; k <= j; k++)
+            {
+                printf("%c", str[k]);
+            }
+            if (!(str[j + 1] == '\0' && i == j))
+                printf(",");
+        }
+    }
+    printf("\n");
+    return 0;
+}
